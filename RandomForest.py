2eeaba49598016bcfc3473834f1dbdd3eaa4aef1
Amino21786 <80826453+Amino21786@users.noreply.github.com>
2021-04-10 16:52:29 +0100

    Completed Metrics (5 metrics)
    Further development from earlier, with the F1 score now implemented (2 versions due to being multi class)
    
    Also for Random Forest, found the best hyperparameters among 50 different combinations :)

--- RandomForest.py
+++ RandomForest.py
@@ -2,12 +2,13 @@ from sklearn.ensemble import RandomForestClassifier
 from metrics import accuracy
 from metrics import confusion_matrix
 from metrics import precision_and_recall
-from metrics import recall
+from metrics import micro_average_f1_score
+from metrics import macro_average_f1_score
 from metrics import false_positive_ratio
 import pandas as pd
 import numpy as np
 import time
-
+from sklearn.metrics import precision_recall_fscore_support, f1_score
 
 
 np.random.seed(42) #seed for repeatable results
@@ -17,7 +18,6 @@ ys=df['price_range'].to_numpy() #The target variable
 
 #forming the train and test data split, through index arrays
 index_array=np.arange(len(ys))
-
 train_index_array = index_array[:int(len(index_array) * 0.8)]
 test_index_array = index_array[int(len(index_array) * 0.8):]
 # get train and test subsets
@@ -25,23 +25,31 @@ x_train = xs[train_index_array]
 y_train = ys[train_index_array]
 x_test = xs[test_index_array]
 y_test = ys[test_index_array]
-start=time.process_time()
+
 # Start to implement the Random Forest Classifier
-RFClassifier = RandomForestClassifier(n_estimators=50)
-RFClassifier.fit(x_train,y_train)
-y_pred_RF = RFClassifier.predict(x_test)
-acc=accuracy(y_pred_RF, y_test) #computes accuracy
-cm=confusion_matrix(y_test, y_pred_RF) #computes test confusion matrix
-print("Accuracy on Test set:", acc)
-print("Confusion Matrix:\n", cm)
-print(precision_and_recall(cm))
+for i in [40,80,90,100]:
+    for j in [1, 2, 4]:
+        start=time.process_time()
+        RFClassifier = RandomForestClassifier(n_estimators=i, random_state=j)
+        RFClassifier.fit(x_train,y_train)
+        y_pred_RF = RFClassifier.predict(x_test)
+        print("n_estimator, random_state: ", i, j)
 
-print("Time Taken:", time.process_time()-start)
+        #5 metrics (Accuracy, confusion Matrix, precision, recall and f1 (both types))
+        acc = accuracy(y_pred_RF, y_test) #computes accuracy
+        cm = confusion_matrix(y_test, y_pred_RF) #computes test confusion matrix
+        prm = precision_and_recall(cm) #computes precision and recall and represents the in a matrix
+        micro_f1 = micro_average_f1_score(cm) #Micro f1, uses the global precision and recall (prone to imbalanced datasets)
+        macro_f1 = macro_average_f1_score(prm) #Macro f1, uses the average of individual classes' precision and recalls (better for imbalanced datasets)
+        print("Accuracy on Test set:", acc)
+        print("Confusion Matrix:\n", cm)
+        print("Precision and Recall Matrix: \n:", prm)
+        print("Micro-average F1 Score:", micro_f1)
+        print("Macro-average F1 Score:", macro_f1)
+        print("Time Taken:", time.process_time()-start) #time taken to compute all of it
 
-# We have played around with the values for train_test_split and when using the 0.7 value, we see the following:
-# The Accuracy on Test with Ranndom Forest is 0.8474723864521747 and it takes roughly 3.5-4 seconds at most to compute this(for the entire data-set, depending on your machine).
-# Later, we thought we can write a for loop which takes different values of train_test_split and proceed to doing the same thing as above. The values for accuracy vary, 
-# the lowest being  0.8333974975938402 for train_test_split=0.5 and the greatest is 0.9429553264604811 for train_test_split=0.99. 
-# However, we can see that for train_test_split values increasing by 0.1, the accuracy values fluctuate between a starting value for 0.7, then decreasing for 0.75,and increasing at a later point for 0.8.
 
-# We will need the time at a later point in the report, where we shall compare the time needed for each method to complete the same test.
+# So I tested the hyperparameters, settled on train test split for 0.8
+#for the RF hyperparameters, n_estimators and random_state, iterated from 10-100 (in 10 intervals for n_estimators) and None - 4 (5 intervals for random_state)
+#Best combination from the 50 trials were n=40 and random_state=4 (best accuracy=0.8825, F1 scores in 0.882-0.883), next one was n=80, random_state=1 (with around 0.880 for f1 and accuracy)
+#Worst combination was n=10 and no random state with around 0.75 for the metrics
--- metrics.py
+++ metrics.py
@@ -9,38 +9,74 @@ import pandas as pd
 
 #measures how many y predictions match the y test set as a percentage
 def accuracy(y_pred, y_test):
-  return np.sum(y_pred==y_test)/y_test.size
+  return np.sum(y_pred == y_test)/y_test.size
 
 def confusion_matrix(Y, Y_predict):
   K = len(np.unique(Y)) #how many unique elements in preidictions and ground truths (here it is 2)
   cm = np.zeros((K, K)) #4x4 matrix
   for i in range(len(Y)):
-    cm[Y_predict[i]][Y[i]]+=1
+    cm[Y_predict[i]][Y[i]] += 1
 
   return cm
 
-# The ratio of correct positive predictions to the total predicted positives
+# The ratio of correct positive predictions to the total predicted positives (precision)
+# The ratio of correct positive predictions to the total positives examples (recall)
 def precision_and_recall(cm): #for 4x4 case
+  p_r_matrix = np.zeros((4,2)) #this will have the 4 classes has rows and the 2 columns as the precision and recall for each class
   for i in range(4):
-    recall=0
-    precision=0
-    fp=0
-    fn=0
-    tp=cm[i][i]
+    recall = 0
+    precision = 0
+    fp = 0
+    fn = 0
+    tp = cm[i][i]
     for j in range(4):
         if i != j:
-          fp+=cm[i][j]
-          fn+=cm[j][i]
-    precision=tp/(tp+fp)
-    recall=tp/(tp+fn)
-    print("Precision for Price Range ", i, " is ",precision)
-    print("Recall for Price Range ", i , " is ", recall)
+          fp += cm[i][j]
+          fn += cm[j][i]
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    p_r_matrix[i][0] = precision
+    p_r_matrix[i][1] = recall
+    
+  return p_r_matrix # 4x2 matrix for the 4 classes and the precision and recall column for each class
 
       
-# The ratio of correct positive predictions to the total positives examples
-def recall(cm):
-  return cm[1][1]/(cm[1][1] + cm[1][0])
-  
+# For the F1 score, since it is multi-class classification, we have to look at a different type of F1 score:
+# Micro-Average F1 Score (looks at global precision and recall from the sums of TP. TN, FP, FN across the classes)
+# This is a global F1 score which is known as the Micro-Average F1 Score (uses the same F1 score formula as binary classification)
+def micro_average_f1_score(cm):
+  fp = 0
+  fn = 0
+  tp = 0
+  for i in range(4):
+    tp += cm[i][i]
+    for j in range(4):
+        if i != j:
+          fp += cm[i][j]
+          fn += cm[j][i]
+  global_precision = tp/(tp+fp)
+  global_recall = tp/(tp+fn)
+
+  return 2*((global_precision*global_recall)/(global_precision+global_recall)) #Micro-Average F1 Score
+
+
+
+#Second type of F1 Score is more helpful when there are imbalanced classes, it takes an average of each precision and recall values for each classes and uses these as the precision and recall
+#values and then use the standard F1 score equation
+def macro_average_f1_score(p_r_matrix):
+  sum_precision = 0
+  sum_recall = 0
+  for i in range(4):
+    sum_precision += p_r_matrix[i][0]
+    sum_recall += p_r_matrix[i][1]
+
+  average_total_precision = sum_precision/4
+  average_total_recall = sum_recall/4
+
+  return 2*((average_total_precision*average_total_recall)/(average_total_precision+average_total_recall)) #Macro-Average F1 Score
+
+
+
 # The probability of falsely rejecting the null hypothesis for a particular test
 # The false positive rate is calculated as the ratio between the number of false positives
 # and the total number of actual negative events (regardless of classification)
