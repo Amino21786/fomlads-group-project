b651403a4e67d33d47d883c2f505d6a54c5ed8e5
AndreeaAlecu19 <81109043+AndreeaAlecu19@users.noreply.github.com>
2021-04-17 21:58:13 +0300

    Previous main+Knn

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import scipy.sparse
 from SoftmaxRegression import SoftmaxRegression
 from RandomForest import RandomForest
+from Knn import Knn
 
 
 def main(ifname):
@@ -12,14 +13,22 @@ def main(ifname):
     accuracy_test=[]
     microf1_scores=[]
     macrof1_scores=[]
+
     acc_RF=[]
     microf1_RF=[]
     macrof1_RF=[]
+
+#Create 3 empty lists, in order to add the values later for the plot.
+    acc_Knnl=[]
+    micro_f1_Knnl=[]
+    macro_f1_Knnl=[]
+
     nvals=[0.5,0.6,0.7,0.8,0.9]
     for n in nvals:
-        acc_train, acc_test, confusion_mat, pr_mat, microf1, macrof1= SoftmaxRegression(ifname, n, 0.001, 
-        iterations=3000, regularisation=1)
+        acc_train, acc_test, confusion_mat, pr_mat, microf1, macrof1= SoftmaxRegression(ifname, n, 0.001, iterations=3000, regularisation=1)
         acc, cm, prm, micro_f1, macro_f1, run_time = RandomForest(ifname, n, 40, 4)
+        acc_Knn, cm_Knn, prm_Knn, micro_f1_Knn, macro_f1_Knn, run_time_Knn = Knn(ifname,n,25)
+
         accuracy_train.append(acc_train)
         accuracy_test.append(acc_test)
         microf1_scores.append(microf1)
@@ -29,21 +38,31 @@ def main(ifname):
         microf1_RF.append(micro_f1)
         macrof1_RF.append(macro_f1)
 
+        acc_Knnl.append(acc_Knn)
+        micro_f1_Knnl.append(micro_f1_Knn)
+        macro_f1_Knnl.append(macro_f1_Knn)
+
     
     plt.figure()
     plt.grid()
+    
     plt.subplot(211)
     plt.xlabel('Train-test splits')
     plt.plot(nvals, accuracy_train, label='Accuracy on train data (Logistic)', color='red')
     plt.plot(nvals, accuracy_test, label='Accuracy on test data (Logistic)', color ='red')
     plt.plot(nvals, acc_RF, label='Accuracy on test data (Random Forest)', color = 'green')
+    plt.plot(nvals,acc_Knnl,label='Accuracy on test data (Knn)', color = 'blue')
     plt.legend()
+
     plt.subplot(212)
     plt.xlabel('Train-test splits')
     plt.plot(nvals, microf1_scores, label='Micro average f1 score (Logistic)', color='red')
     plt.plot(nvals, macrof1_scores, label= 'Macro average f1 score (Logistic)', color='red')
     plt.plot(nvals, macrof1_RF, label= 'Macro average f1 score (Random Forest)', color='green')
     plt.plot(nvals, microf1_RF, label= 'Micro average f1 score (Random Forest)', color='green')
+    plt.plot(nvals, macro_f1_Knnl, label= 'Macro average f1 score (Knn)', color='yellow')
+    plt.plot(nvals, micro_f1_Knnl, label= 'Micro average f1 score (Knn)', color='black')
+
     plt.plot()
     plt.legend()
     plt.show()
