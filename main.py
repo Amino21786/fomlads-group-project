523b5fe6ce221936e65c09804c13920bb0110408
kmicek <77343325+kmicek@users.noreply.github.com>
2021-04-18 11:57:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import scipy.sparse
 from SoftmaxRegression import SoftmaxRegression
 from RandomForest import RandomForest
 from Knn import Knn
+from LDA import LDA
 
 
 def main(ifname):
@@ -23,11 +24,16 @@ def main(ifname):
     micro_f1_Knnl=[]
     macro_f1_Knnl=[]
 
+    acc_LDA =[]
+    micro_f1_LDA=[]
+    macro_f1_LDA=[]
+
     nvals=[0.5,0.6,0.7,0.8,0.9]
     for n in nvals:
         acc_train, acc_test, confusion_mat, pr_mat, microf1, macrof1= SoftmaxRegression(ifname, n, 0.001, iterations=3000, regularisation=1)
         acc, cm, prm, micro_f1, macro_f1, run_time = RandomForest(ifname, n, 40, 4)
         acc_Knn, cm_Knn, prm_Knn, micro_f1_Knn, macro_f1_Knn, run_time_Knn = Knn(ifname,n,25)
+        accLDA, microf1_LDA, macrof1_LDA, runtime_LDA = LinearDiscriminantAnalysis(ifname, n)
 
         accuracy_train.append(acc_train)
         accuracy_test.append(acc_test)
@@ -42,6 +48,10 @@ def main(ifname):
         micro_f1_Knnl.append(micro_f1_Knn)
         macro_f1_Knnl.append(macro_f1_Knn)
 
+        acc_LDA.append(accLDA)
+        micro_f1_LDA.append(microf1_LDA)
+        macro_f1_LDA.append(macrof1_LDA)
+
     
     plt.figure()
     plt.grid()
@@ -52,6 +62,7 @@ def main(ifname):
     plt.plot(nvals, accuracy_test, label='Accuracy on test data (Logistic)', color ='red')
     plt.plot(nvals, acc_RF, label='Accuracy on test data (Random Forest)', color = 'green')
     plt.plot(nvals,acc_Knnl,label='Accuracy on test data (Knn)', color = 'blue')
+    plt.plot(nvals,acc_LDA, label='Accuracy on test data (LDA)', color ='yellow')
     plt.legend()
 
     plt.subplot(212)
@@ -62,6 +73,8 @@ def main(ifname):
     plt.plot(nvals, microf1_RF, label= 'Micro average f1 score (Random Forest)', color='green')
     plt.plot(nvals, macro_f1_Knnl, label= 'Macro average f1 score (Knn)', color='yellow')
     plt.plot(nvals, micro_f1_Knnl, label= 'Micro average f1 score (Knn)', color='black')
+    plt.plot(nvals, macro_f1_LDA, label= 'Macro average f1 score (LDA)', color='pink')
+    plt.plot(nvals, micro_f1_LDA, label= 'Micro average f1 score (LDA)', color='purple')
 
     plt.plot()
     plt.legend()
