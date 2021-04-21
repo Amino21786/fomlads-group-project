c9e483f302a18ccb14dc36c5e1360e4ae58e96fd
Amino21786 <80826453+Amino21786@users.noreply.github.com>
2021-04-21 14:45:20 +0100

    changes to plots to make it work

--- plots.py
+++ plots.py
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")
 
 def rf_hyperparameters(dataset):
     X_train, Y_train, X_test, Y_test = train_test_data(dataset, 0.8) #runs train_test_split function
-    n_range=range(10, 100, 2)
+    n_range=range(10, 100)
     acc_scores=[]
     mi_f1=[]
     ma_f1=[]
@@ -55,7 +55,7 @@ def rf_hyperparameters(dataset):
 
 def oob_error_rf(dataset):
     X_train, Y_train, X_test, Y_test = train_test_data(dataset, 0.8) #runs train_test_split function
-    n_range=range(15,100, 2)
+    n_range=range(15,100)
     oob_errors=[]
     for i in n_range:
         RFClassifier = RandomForestClassifier(n_estimators=i, oob_score=True, random_state=4)
@@ -67,6 +67,7 @@ def oob_error_rf(dataset):
     plt.xlabel('Number of trees (n_estimators)')
     plt.ylabel('OOB Error')
     plt.savefig('Random Forest Loss Function Graph')
+    plt.close()
     
  
 # not scaled, below:
@@ -146,33 +147,3 @@ oob_error_rf('MobilePricingUpdated.csv')
 
 
 
-#tried to use the code from the main py as a function but stuck, for now will have this plotting idea as it is in the main file and for the separate model plots can have the in this file
-"""
-def accuracy_graph(model):
-    nvals=[0.5,0.6,0.7,0.8,0.9]
-    models=['Random Forest', 'KNN']
-    fig=plt.figure()
-    plt.xlabel('Train-test splits')
-    plt.title('Accuracy vs Train-test splits')
-    fig_2=plt.figure()
-    plt.xlabel('Train-test splits')
-    plt.title('F1 Score vs Train-test splits')
-
-    for i in model:
-        accuracy_test=[]
-        microf1_scores=[]
-        macrof1_scores=[]
-        for n in nvals:
-            acc_test, confusion_mat, pr_mat, microf1, macrof1, run_time = model[i]
-            accuracy_test.append(acc_test)
-            microf1_scores.append(microf1)
-            macrof1_scores.append(macrof1)
-        fig += plt.plot(nvals, acc_test, label='Accuracy on test data ('+models[i]+')')
-        plt.legend()
-        
-        plt.plot(nvals, microf1_scores, label='Micro average f1 score ('+models[i]+')')
-        plt.plot(nvals, macrof1_scores, label= 'Macro average f1 score ('+models[i]+')')
-        plt.plot()
-        plt.legend()
-   
-"""
